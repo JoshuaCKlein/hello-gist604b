163ad11794829960723f04daa01daab5ba4c13a4
Joshua Klein <98036986+JoshuaCKlein@users.noreply.github.com>
2025-11-01 20:25:58 -0700

    Update hello.py

--- scripts/hello.py
+++ scripts/hello.py
@@ -3,20 +3,20 @@
 GIST 604B - Open Source GIS
 Module 1 Assignment 3: GitHub Repository Management
 
-Student: [YOUR NAME HERE]
-Date: [TODAY'S DATE]
+Student: Joshua Klein
+Date: 11/1/2025
 University of Arizona
 """
 
 def main():
     """Simple hello world function for GIST 604B"""
     print("Hello from GIST 604B!")
-    print(f"Student: [YOUR NAME HERE]")
+    print(f"Student: Joshua Klein")
     print("Learning open source GIS workflows with GitHub")
     print("University of Arizona coordinates: 32.2319° N, 110.9501° W")
     
     # Add your own message here!
-    print("[ADD YOUR OWN MESSAGE ABOUT WHY YOU'RE INTERESTED IN GIS]")
+    print("I have always had trouble picking a field of study and I settled on GIS because it gives me the opportunity to work with data from many different disciplines.")
 
 if __name__ == "__main__":
     main()
